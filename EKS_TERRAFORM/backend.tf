566baab98b1c94b1531ed6c6e12bac4bfc63e275
prashantsuk <42904978+prashantsuk@users.noreply.github.com>
2024-03-17 23:23:16 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    //bucket = "prashantbucketrandam" # Replace with your actual S3 bucket name
-      bucket = "anandrandamtest" #anand targe s3  bucket
+    bucket = "prashantbucketrandam" # Replace with your actual S3 bucket name
+      //bucket = "anandrandamtest" #anand targe s3  bucket
     //bucket = "abdul30jan24"
     key    = "EKS/terraform.tfstate"
-    //region = "us-west-2" 
-     region = "us-east-1"
+    region = "us-west-2" 
+     //region = "us-east-1"
   }
 }
